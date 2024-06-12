48f667b0d88288fe1fba7a12512b8901e73821ca
{{nielchaudhary}} <119964539+nielchaudhary@users.noreply.github.com>
2024-06-13 02:00:56 +0530

    fixed imports and added handler route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
-import { createPcapSession } from "./utils/packetCapture";
+import { latencyManagement } from "./utils/latencyManagement";
+import { createPcapSession, getDeviceInventory } from "./utils/deviceInventory";
 
 const app = express();
 
 createPcapSession();
+latencyManagement();
 
+
+app.get('/devices', (req, res) => {
+    res.json(getDeviceInventory());
+});
 app.listen(3000);
